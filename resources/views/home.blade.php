fb12a48417b662a011bdda1b58ef1c4c027491d9
Yaqin1 <ahmadnuryaqin05@gmail.com>
2022-08-23 12:16:20 +0700

    menambahkan halaman home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends ('layout.app')
+
+@section ('title')
+    Home
+@endsection
+
+@section ('content')
+    <div class="row g-10 text-center">
+        <div class="col-3">
+        <div class="p-3  bg-warning mt-4">10 Barang  <i class="fa-solid fa-box"></i></div>
+        </div>
+
+        <div class="col-3">
+        <div class="p-3  bg-warning mt-4">15 kategori  <i class="fa-solid fa-tags"></i></div>
+        </div>
+
+        <div class="col-3">
+        <div class="p-3  bg-warning mt-4">50 Suplier  <i class="fa-solid fa-plane"></i></div>
+        </div>
+
+        <div class="col-3">
+        <div class="p-3 bg-warning mt-4">500 Pembeli  <i class="fa-solid fa-users"></i></div>
+        </div>
+</div>
+
+
+@endsection
